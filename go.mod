38b4ad2b6991af1821b8160ccf9c45b9f8d61c22
marin-h <marinahuberman@gmail.com>
2020-08-29 19:36:45 -0300

    add createBid function and uuid generation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marin-h/simple-dsp
 
 go 1.15
+
+require github.com/google/uuid v1.1.1
--- internal/bid.go
+++ internal/bid.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"github.com/google/uuid"
+)
+
 type Bid struct {
 	Id        string `json:"-"`
 	UserId    string `json:"-"`
 	Timestamp int64  `json:"-"`
 	Price     float64
 	Nurl      string
+}
+
+func createBid(id string, userId string, price float64, timestamp int64) *Bid {
+	return &Bid{id, userId, timestamp, price, "/impression"}
+}
+
+func getBidId() string {
+	return uuid.New().String()
 }
