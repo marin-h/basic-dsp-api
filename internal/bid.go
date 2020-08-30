c86eda0fa0407ba36bf76c8ef8dd5d77b181a25b
marin-h <marinahuberman@gmail.com>
2020-08-30 14:18:00 -0300

    refactor bid struct removing nurl field

--- internal/bid.go
+++ internal/bid.go
@@ -7,13 +7,13 @@ import (
 type Bid struct {
 	Id        string  `json:"-"`
 	UserId    string  `json:"-"`
-	Timestamp int64   `json:"-"`
+	Timestamp int64   `json:"timestamp"`
+	Status    string  `json:"-"`
 	Price     float64 `json:"price"`
-	Nurl      string  `json:"nurl"`
 }
 
 func createBid(id string, userId string, price float64, timestamp int64) *Bid {
-	return &Bid{id, userId, timestamp, price, "/bid/" + id}
+	return &Bid{id, userId, timestamp, "pending", price}
 }
 
 func getBidId() string {
